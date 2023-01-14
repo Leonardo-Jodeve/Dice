e32c055a0cf40b66477174084fc697dec35796f9
Leonardo-Jodeve <dszw2000@gmail.com>
2023-01-14 23:28:12 +0800

    modify sql script

--- src/Dice.sql
+++ src/Dice.sql
@@ -2,13 +2,14 @@ CREATE DATABASE Dice;
 
 USE Dice;
 
+DROP TABLE user;
 CREATE TABLE user
 (
     id BIGINT(20) PRIMARY KEY auto_increment,
     username varchar(64) UNIQUE
 );
 
-
+DROP TABLE dice_history;
 CREATE TABLE dice_history
 (
     left_username VARCHAR(64),
@@ -19,6 +20,8 @@ CREATE TABLE dice_history
     time datetime DEFAULT NOW()
 );
 
+DELETE FROM dice_history where left_sum <> 100; -- 删除全表。这条语句中的条件无意义，只是为了让解释器认为语句安全
+DELETE FROM user where id <> -1; -- 删除全表。这条语句中的条件无意义，只是为了让解释器认为语句安全
 INSERT INTO user VALUES
 (null, 'admin'),
 (null, 'user');
